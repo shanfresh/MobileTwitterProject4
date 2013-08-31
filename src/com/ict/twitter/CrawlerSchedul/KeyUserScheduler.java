2c7e08c546f545f147eaa0540e35a9286330aa67
shanfresh <networktest1@126.com>
2013-08-31 17:25:17 +0800

    暂时保存-施工、

--- src/com/ict/twitter/CrawlerSchedul/KeyUserScheduler.java
+++ src/com/ict/twitter/CrawlerSchedul/KeyUserScheduler.java
@@ -1,19 +1,66 @@
 package com.ict.twitter.CrawlerSchedul;
 
+import java.util.Iterator;
 import java.util.TimerTask;
+import java.util.Vector;
+
+import org.junit.Test;
 
 import com.ict.twitter.CrawlerServer.CrawlerServer;
+import com.ict.twitter.DAO.DBKeyUserDAO;
+import com.ict.twitter.DAO.bean.KeyUser;
+import com.ict.twitter.tools.BasePath;
+import com.ict.twitter.tools.ReadTxtFile;
 //
 public class KeyUserScheduler extends TimerTask{
 	private CrawlerServer crawlserver;
+	private DBKeyUserDAO dbkeyuser;
+	private String basepath=BasePath.getBase();
+	private String FilePath=basepath+"/UsefulFile/KeyIDs.txt";
+	
+	public KeyUserScheduler(){
+		
+	}
 	public KeyUserScheduler(CrawlerServer _crawlserver){
 		this.crawlserver=_crawlserver;
+		this.dbkeyuser=new DBKeyUserDAO();
 	}
 	@Override
 	public void run() {
 		
 		
 	}
+	public Vector<KeyUser> getAllKeyUsers(){
+		Vector<KeyUser> all=new Vector<KeyUser>();
+		all.addAll(GetTextUser());
+		all.addAll(GetDBUser());
+		return all;
+
+	}
+	private Vector<KeyUser> GetDBUser(){
+		Vector<KeyUser> dbUser=dbkeyuser.GetKeyUser();
+		return dbUser;
+	}
+	public Vector<KeyUser> GetTextUser(){
+		Vector<KeyUser> all=new Vector<KeyUser>();
+		ReadTxtFile rxf=new ReadTxtFile(FilePath);
+		Vector<String> allTxtName=rxf.read();
+		Iterator<String> it=allTxtName.iterator();
+		while(it.hasNext()){
+			String name=it.next();
+			KeyUser keyUser=new KeyUser(name,0,0);
+			all.add(keyUser);
+		}
+		return all;
+	}
+	
+	@Test
+	public void doText(){
+		KeyUserScheduler t=new KeyUserScheduler(null);
+		Vector mm=t.getAllKeyUsers();
+		System.out.println(mm.size());
+	}
+	
 
 
 }
--- src/com/ict/twitter/CrawlerSchedul/KeyWordsScheduler.java
+++ src/com/ict/twitter/CrawlerSchedul/KeyWordsScheduler.java
@@ -2,12 +2,19 @@ package com.ict.twitter.CrawlerSchedul;
 
 import com.ict.twitter.CrawlerServer.CrawlerServer;
 
-public class KeyWordsScheduler extends BasicScheduler{
+public class KeyWordsScheduler extends BasicScheduler implements Runnable{
 
 	public KeyWordsScheduler(CrawlerServer _crawlserver) {
 		super(_crawlserver);
 		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public void run() {//平均每天应该对关键词进行过滤一次
+		
+		
+	}
+	
 
 
 }
